c75340d9eb569e4469f4c8dd98615f9821708b21
aziza <47671886+azizaElsayed@users.noreply.github.com>
2019-08-16 09:20:26 -0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrainingCenterMVC.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using TrainingCenterMVC.Models;
+
+[assembly: OwinStartupAttribute(typeof(TrainingCenterMVC.Startup))]
+namespace TrainingCenterMVC
+{
+    public partial class Startup
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateUsers();
+        }
+
+        public void CreateUsers()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+
+            var User = new ApplicationUser();
+            User.Email = "aziza22@gmail.com";
+            User.UserName = "Aziza";
+            var check = userManager.Create(User, "aziza95@gmail.com");
+            if (check.Succeeded)
+            {
+              
+                userManager.AddToRole(User.Id, "admin");
+                
+            }
+
+     
+
+
+
+
+        }
+
+    }
+}
